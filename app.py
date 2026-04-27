79116c35ee00a5543d954ac3511056049fce156b
Rafiq0092 <rafiq.220111415@gehu.ac.in>
2026-04-27 12:28:49 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -237,4 +237,5 @@ def reload_model():
 
 if __name__ == "__main__":
     load_model_once()
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
